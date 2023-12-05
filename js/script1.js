972ac20369e5ab317bbe381a8e03202bf3888afa
leonardoct3 <leocarvalhoteixeira@gmail.com>
2023-12-05 11:29:32 -0300

    ajuste

--- js/script1.js
+++ js/script1.js
@@ -3,15 +3,17 @@ document.addEventListener('DOMContentLoaded', function(){
     const input = document.querySelector('.padd');
 
     form.addEventListener('submit', function(event) {
-        event.preventDefault(); 
+        event.preventDefault(); // Previne o comportamento padrão do envio do formulário
         
         const escrita = input.value;
         
         if (escrita !== 'Digite aqui...') {
-            
+            // Save to localStorage apenas se o valor for diferente de 'Digite aqui...'
             localStorage.setItem('userInput', escrita);
-            
+            // Ou execute outras ações com o valor do input
 
+            // Aqui você pode redirecionar para outra página, se desejar:
+            window.location.href = "fluxo_de_tela2.3.html";
         }
     });
 });
